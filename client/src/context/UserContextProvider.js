6be08bb913b89b5cd46e4e9e17e8bc1ac6ebf030
Kevin Kobus <kevinkobus@gmail.com>
2023-07-26 12:19:58 -0600

    commit of progress on lessons

--- client/src/context/UserContextProvider.js
+++ client/src/context/UserContextProvider.js
@@ -1,4 +1,5 @@
 import React, { useState, createContext } from "react"
+import axios from "axios"
 
 
 const UserContext = createContext()
@@ -12,10 +13,28 @@ function UserContextProvider(props){
 
     const [userState, setUserState] = useState(initState)
 
+    function signup(credentials){
+        axios.post("/auth/signup", credentials)
+        .then(res => console.log(res))
+        // .catch(err => console.dir(err))
+        .catch(err => console.log(err.response.data.errMsg))
+    }
+
+    function login(credentials) {
+        axios.post("/auth/login", credentials)
+        .then(res => console.log(res))
+        // .catch(err => console.dir(err))
+        .catch(err => console.log(err.response.data.errMsg))
+    }
+
+
+
     return (
         <UserContext.Provider
         value={{
-            ...userState
+            ...userState,
+            signup,
+            login
         }}>
             { props.children }
         </UserContext.Provider>
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import "./css/styles.css"
-import UserContext from "./context/UserContextProvider"
+import { UserContextProvider } from "./context/UserContextProvider"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserContext>
+    <UserContextProvider>
       <App />
-    </UserContext> 
+    </UserContextProvider> 
     </BrowserRouter>
   </React.StrictMode>
 );
