043d8a2a23446adb389d6b268ae0c2408e2aa0ed
Kevin Kobus <kevinkobus@gmail.com>
2023-08-12 12:02:17 -0600

    saving progress through second to last lesson of L6 part 3

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ export default function App(){
   const { token, logout } = useContext(UserContext)
   return (
     <div className="app">
-      <Navbar logout={logout} />
+      {token && <Navbar logout={logout} token={token}/>}
       <Routes>
         <Route 
           path="/" 
